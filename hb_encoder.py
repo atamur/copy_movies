7f0d4adc0b448817b73d10898906385aa9c698f5
atamur <atamur@gmail.com>
2013-04-13 09:41:17 +0200

    add comment

--- hb_encoder.py
+++ hb_encoder.py
@@ -1,15 +1,11 @@
 __author__ = 'looser'
 
-# hb cli:
-# -i "D:\temp\MOV042.MOD" -t 1 -c 1
-# -o "D:\temp\processed\MOV042.m4v"
-# -f mp4 -4  --detelecine --decomb --denoise="weak" -w 1024 -l 576
-# -e x264 -q 19 --vfr  -a 1 -E copy:ac3 -B 0 -6 auto -R Auto -D 0 --gain=0 --audio-copy-mask none --audio-fallback ffac3 -x b-adapt=2:rc-lookahead=50 --verbose=1
-
 from subprocess import call
 
 class HbEncoder:
-
+    """
+    Uses CLI of HandBrake to encode videos from JVC camera that were shoot with 16x9 setting.
+    """
     def encode(self, source, target):
         cmd = ("C:/Program Files/Handbrake/HandBrakeCLI.exe "
                + "-i \"" + source + "\" -t 1 -c 1 "

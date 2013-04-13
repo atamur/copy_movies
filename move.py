c4427a7bd9080ae0850dbd5a5886ecf37722ce23
atamur <atamur@gmail.com>
2013-04-13 09:47:32 +0200

    fix inspections

--- move.py
+++ move.py
@@ -1,5 +1,3 @@
-__author__ = 'looser'
-# -s "mod,avi,mp4,mov,3gp,m4v,asf"
 from optparse import OptionParser
 import os
 import shutil
@@ -21,19 +19,19 @@ destination = args[1]
 def find_date_folder(path):
     if not path.startswith(source):
         return None
-    (parent, dir) = os.path.split(path)
-    if re.match("\d{4}", dir):
-        return dir
+    (parent, folder) = os.path.split(path)
+    if re.match("\d{4}", folder):
+        return folder
     return find_date_folder(parent)
 
 for root, dirs, files in os.walk(source):
     for file in files:
         (unused, ext) = os.path.splitext(file)
         if ext.lower() in extensions:
-            dir = find_date_folder(root)
+            folder = find_date_folder(root)
             src = os.path.join(root, file)
-            if dir == None: dst = os.path.join(destination, "misc")
-            else: dst = os.path.join(destination, dir)
+            if folder is None: dst = os.path.join(destination, "misc")
+            else: dst = os.path.join(destination, folder)
             os.makedirs(dst, exist_ok=True)
             shutil.move(src, dst)
             print("{0} => {1}".format(src, dst))

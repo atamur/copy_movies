cc40ced819ae8e3bec5c11c9f731ed546122f328
Ilya <atamur@gmail.com>
2022-07-19 23:01:32 +0200

    revolut csv convertor

---
+++ revolut_csv_to_csv.py
@@ -0,0 +1,45 @@
+import csv, time
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+def escape(str):
+  return ''.join([x if ord(x) < 255 else '?' for x in str])
+
+fname = 'account-statement.csv'
+df = pd.read_csv(fname)
+df = df[df['State'] == 'COMPLETED']
+
+transactionsConverted = open(fname + '.csv', 'w', encoding ="ISO-8859-1")
+writer = csv.writer(transactionsConverted)
+
+# Write header row
+writer.writerow(['Date','Payee','Category','Memo','Outflow','Inflow'])
+
+for index, row in df.iterrows():
+  print(row)
+  date = time.strftime('%d/%m/%Y', time.strptime(row['Started Date'], '%Y-%m-%d %H:%M:%S')) # DD/MM/YYYY
+  payee = row['Description']
+  category = ''
+  memo = ''
+  amount = row['Amount'] - row['Fee']
+
+  amount = round(amount, 2)
+
+  if amount == 0:
+    continue # skip empty
+
+  if payee == 'To CHF':
+    continue # skip transactions between current and savings
+
+  # Check if the transaction is Debit (outflow) or Credit (inflow)
+  if amount >= 0:
+    outflow = 0
+    inflow = amount
+  else:
+    outflow = -1 * amount
+    inflow = 0
+
+  writer.writerow([date, escape(payee), category, memo, outflow, inflow])
+
+transactionsConverted.close()
